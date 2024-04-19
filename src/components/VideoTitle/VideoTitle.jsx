83eae3b73bfde0105d9b47255c3820a5831f4eeb
Rafael <tauschek.rafael@gmail.com>
2024-04-19 15:51:37 +0200

    added Genres-Menu in VideoTitle, some minimalistic responsiveness in Header

--- src/components/VideoTitle/VideoTitle.jsx
+++ src/components/VideoTitle/VideoTitle.jsx
@@ -1,17 +1,63 @@
-import React from 'react';
-import styles from './VideoTitle.module.css';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import React, { useEffect, useRef, useState } from "react";
+import styles from "./VideoTitle.module.css";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 export default function VideoTitle() {
-    return (
-        <div className={styles.TitleContainer}>
-            <span className={styles.Title}>Movies</span>
-            <div className={styles.Main}>
-                <div className={styles.GenresContainer}>
-                <span className={styles.GenresTitle}>Genres</span>
-                {/* <ArrowDropDownIcon/> */}
-                </div>
-            </div>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const genresContainerRef = useRef(null);
+  const handleGenresClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleClickOutside = (event) => {
+    if (
+      genresContainerRef.current &&
+      !genresContainerRef.current.contains(event.target)
+    ) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  const handleSelectGenreClick = () => {
+    /**
+     * TODO:
+     * only show Videos of the Category
+     * maybe add a "red '|' left of the Genre-name to show the currently selected Genre "
+     */
+  }
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
+  return (
+    <div className={styles.TitleContainer}>
+      <span className={styles.Title}>Movies</span>
+      <div>
+        <div
+          className={styles.GenresContainer}
+          onClick={handleGenresClick}
+          ref={genresContainerRef}
+        >
+          <span className={styles.GenresTitle}>Genres</span>
+          {/* <ArrowDropDownIcon/> */}
         </div>
-    );
+        {isMenuOpen && (
+          <div className={styles.GenresMenu}>
+            <span onClick={handleSelectGenreClick} className={styles.GenresMenu__Item}>Western</span>
+            <span onClick={handleSelectGenreClick} className={styles.GenresMenu__Item}>Comedy</span>
+            <span onClick={handleSelectGenreClick} className={styles.GenresMenu__Item}>Horror</span>
+            <span onClick={handleSelectGenreClick} className={styles.GenresMenu__Item}>Action</span>
+            <span onClick={handleSelectGenreClick} className={styles.GenresMenu__Item}>Drama</span>
+            <span onClick={handleSelectGenreClick} className={styles.GenresMenu__Item}>Thriller</span>
+            <span onClick={handleSelectGenreClick} className={styles.GenresMenu__Item}>Adventure</span>
+            <span onClick={handleSelectGenreClick} className={styles.GenresMenu__Item}>Animated</span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
