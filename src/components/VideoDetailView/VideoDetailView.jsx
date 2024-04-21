4e934e9f1d55dac17103b9af5a4331170a613833
Rafael <tauschek.rafael@gmail.com>
2024-04-21 19:30:44 +0200

    some additional conent and styling for videodetailview

--- src/components/VideoDetailView/VideoDetailView.jsx
+++ src/components/VideoDetailView/VideoDetailView.jsx
@@ -1,13 +1,9 @@
 import React, { useEffect, useState } from "react";
 import styles from "./VideoDetailView.module.css";
-<<<<<<< HEAD
-import { get } from "../../services/http";
+import { get } from "../../services/HTTPS/http";
 import CloseIcon from "@mui/icons-material/Close";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import AddIcon from "@mui/icons-material/Add";
-=======
-import { get } from "../../services/HTTPS/http";
->>>>>>> 126528bf97311fce5706085c963398947a532cd7
 
 export default function VideoDetailView({ video, handleBackButtonClick }) {
   const BASE_URL = "http://127.0.0.1:8000/";
@@ -42,6 +38,7 @@ export default function VideoDetailView({ video, handleBackButtonClick }) {
       {videoDetail && Object.keys(videoDetail).length > 0 && (
         <div className={styles.VideoDetail} style={{ backgroundImage: `url(${"http://127.0.0.1:8000" + videoDetail.video.thumbnail})`,}}
         >
+          
           <div className={styles.GradientOverlay}></div>
           <div className={styles.BackButton}>
             <CloseIcon onClick={handleBackClick} />
@@ -62,7 +59,7 @@ export default function VideoDetailView({ video, handleBackButtonClick }) {
             {videoDetail.video.title}
           </span>
           <span className={styles.VideoItem__Description}>
-            {videoDetail.video.description}
+            {videoDetail.video.short_description}
           </span>
           <div className={styles.VideoItem__ButtonContainer}>
             <div className={styles.ButtonContainer__Play}>
@@ -77,6 +74,22 @@ export default function VideoDetailView({ video, handleBackButtonClick }) {
             <div className={styles.Footer__Header}>
               <span className={styles.Header__Title}>DETAILS</span>
             </div>
+            <div className={styles.VideoItem_Footer_Content}>
+                <div className={styles.VideoItem__VideoDetail__Main}>
+                  <span className={styles.VideoDetail__Main__Title}>{videoDetail.video.title}</span>
+                  <span className={styles.VideoDetail__Main__Description}>{videoDetail.video.long_description}</span>
+                </div>
+                <div className={styles.VideoItem__VideoDetails}>
+                  <span className={styles.VideoDetail_Title}>Duration:</span>
+                  <span className={styles.VideoDetail_Content}>{videoDetail.video.duration_time}</span>
+                  <span className={styles.VideoDetail_Title}>Relese date:</span>
+                  <span className={styles.VideoDetail_Content}>{videoDetail.video.release_year}</span>
+                  <span className={styles.VideoDetail_Title}>Genre:</span>
+                  <span className={styles.VideoDetail_Content}>{videoDetail.video.genre}</span>
+                  <span className={styles.VideoDetail_Title}>Age restriction:</span>
+                  <span className={styles.VideoDetail_Content}>{videoDetail.video.fsk}</span>
+                </div>
+            </div>
           </div>
 
         </div>
@@ -84,3 +97,5 @@ export default function VideoDetailView({ video, handleBackButtonClick }) {
     </div>
   );
 }
+
+
