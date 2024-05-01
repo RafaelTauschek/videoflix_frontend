c86908aae45bf82e46b4ded5bdebf1f9159cbe86
Rafael <tauschek.rafael@gmail.com>
2024-05-01 17:49:55 +0200

    some adjustments

--- src/pages/Admin Page/AdminPage.jsx
+++ src/pages/Admin Page/AdminPage.jsx
@@ -134,7 +134,7 @@ export default function AdminPage() {
     // },
 
     onSubmit: (values, { setSubmitting, setErrors }) => {
-      const BASE_URL = "http://127.0.0.1:8000/videos/";
+      const BASE_URL = "http://rafael-max.developerakademie.org/videos/";
       const formData = new FormData();
       formData.append("title", values.title);
       formData.append("short_description", values.short_description);
